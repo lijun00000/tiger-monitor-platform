d5d4c3f5e3763ae9cf5b238dc1f5a5949b3b33c7
wangdong <wangdong@ tiger .com>
2019-11-09 16:19:36 +0800

    运维监控平台

---
+++ src/main/java/com/tiger/monitor/platform/tigermonitorplatform/TigerMonitorPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tiger.monitor.platform.tigermonitorplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerMonitorPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TigerMonitorPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiger/monitor/platform/tigermonitorplatform/TigerMonitorPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiger.monitor.platform.tigermonitorplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerMonitorPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
